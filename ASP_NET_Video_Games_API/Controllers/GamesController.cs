a7b468c4afc6041856f42007f69162941e162b91
urivat <rashad.porter18@gmail.com>
2022-04-26 12:38:02 -0400

    added all files to the sql Database and created controller for endpoints.

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,35 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    // api/games
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetGames()
+        {
+            var videoGameNames = _context.VideoGames.Select(vg => vg.Name).Distinct();
+
+            return Ok(videoGameNames);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGamesById(int id)
+        {
+            var videoGames = _context.VideoGames.Where(vg => vg.Id == id);
+            return Ok(videoGames);
+        }
+
+    }
+}
